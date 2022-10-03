e30426d777264bd4e7f41178c1adaee84f3ffbd7
Leonel <lelobo75@gmail.com>
2022-10-03 09:24:00 +0200

    first commit

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { Head, Link } from "@inertiajs/inertia-vue3";
+import FrontendLayout from "@/Layouts/Frontend.vue";
+import Hero from "@/Components/Frontend/Hero.vue";
+import Promote from "@/Components/Frontend/Promote.vue";
+import About from "@/Components/Frontend/About.vue";
+import Skills from "@/Components/Frontend/Skills.vue";
+import Portfolio from "@/Components/Frontend/Portfolio.vue";
+import Service from "@/Components/Frontend/Service.vue";
+import ContactMe from "@/Components/Frontend/ContactMe.vue";
+
+defineProps({
+  skills: Object,
+  projects: Object,
+});
+</script>
+
+<template>
+  <Head title="Welcome" />
+  <FrontendLayout>
+    <!-- Hero primary -->
+    <Hero />
+    <!-- Promote tail-100 -->
+    <Promote />
+    <!-- About -->
+    <About />
+    <!-- Skills tail-100 -->
+    <Skills :skills="skills" />
+    <!-- Portafolio primary -->
+    <Portfolio :skills="skills" :projects="projects" />
+    <!-- Services primary -->
+    <Service />
+    <!-- ContactMe  -->
+    <ContactMe />
+  </FrontendLayout>
+</template>
---
+++ routes/web.php
@@ -0,0 +1,49 @@
+<?php
+
+use App\Http\Controllers\ContactController;
+use Inertia\Inertia;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Application;
+use App\Http\Controllers\SkillController;
+use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\WelcomeController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return Inertia::render('Welcome', [
+//         'canLogin' => Route::has('login'),
+//         'canRegister' => Route::has('register'),
+//         'laravelVersion' => Application::VERSION,
+//         'phpVersion' => PHP_VERSION,
+//     ]);
+// });
+
+
+Route::get('/', [WelcomeController::class, 'welcome'])->name('welcome');
+Route::post('/contact', ContactController::class)->name('contact');
+
+Route::middleware(['auth', 'verified'])->group(function(){
+    Route::get('/dashboard', function(){
+      return Inertia::render('Dashboard');
+    })->name('dashboard');
+  
+    Route::resource('/skills',SkillController::class);
+    Route::resource('/projects',ProjectController::class);
+  
+  });
+
+// Route::get('/dashboard', function () {
+//     return Inertia::render('Dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+
+require __DIR__.'/auth.php';
